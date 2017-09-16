2dbc57384ef706d2b2e569f4fbf97cb4eca59ebb
jfc4050 <jfchiu@uw.edu>
2017-09-16 11:58:37 -0700

    9/16/17 11:58AM fixed quicksort

--- BinarySearchTest.py
+++ BinarySearchTest.py
@@ -1,26 +1,30 @@
 def main():
-    testlist = [0, 1, 2, 8, 13, 17, 19, 32, 42]
-    test_query = 31
-    print(binary_search(testlist, 0, len(testlist) - 1, test_query))
+    test_query = int(input("enter test_query\n"))
+    test_list = [0, 5, 20, 50, 66, 100, 150, 172]
+    first = 0
+    last = len(test_list) - 1
+
+    print(binary_search(test_query, test_list, first, last))
 
 
-def binary_search(list_in, first, last, query):
-    mid = (first + last) // 2
+def binary_search(query_in, list_in, first, last):
 
-    if list_in[mid] == query:
-        return mid
-
+    # stop recursion if list size = 0
     if first == last:
-        return "not found"
+        return str(query_in) + " not found"
 
+    mid = (first + last) // 2
+    # if query_in is found
+    if list_in[mid] == query_in:
+        return mid
     else:
-        # if query is in first half of list
-        if query < list_in[mid]:
-            return binary_search(list_in, first, mid, query)
+        # if query_in is in first half of list
+        if query_in < list_in[mid]:
+            return binary_search(query_in, list_in, first, mid)
 
-        # if query is in second half of list
-        if query > list_in[mid]:
-            return binary_search(list_in, mid+1, last, query)
+        # if query_in is in second half of list
+        if query_in > list_in[mid]:
+            return binary_search(query_in, list_in, mid+1, last)
 
 
 main()
--- QuickSort.py
+++ QuickSort.py
@@ -1,23 +1,33 @@
+def swap(array, index1, index2):
+    # swap values of indices 1 and 2 in array
+    mem = array[index1]
+    array[index1] = array[index2]
+    array[index2] = mem
+
+
+def quick_sort(array, left, right):
+    if (right - left) > 0:
+        pivot = array[left]
+        # pivot_boundary = array.index(pivot) + 1
+        pivot_boundary = left + 1
+
+        for i in range(pivot_boundary, right + 1):
+            if array[i] < pivot:
+                swap(array, i, pivot_boundary)
+                pivot_boundary += 1
+        # put pivot in final position (behind pivot boundary)
+        swap(array, array.index(pivot), pivot_boundary - 1)
+
+        # recurse on left and right partitions
+        quick_sort(array, left, pivot_boundary - 1)
+        quick_sort(array, pivot_boundary, right)
+
+
 def main():
-    array_in = [3, 8, 2, 5, 1, 4, 7, 6]
+    array_in = [3, 8, 2, 5, 1, 4, 7, 6, 10, 3, 2]
     left = 0
-    right = 7
-    quickSort(array_in, left, right)
-    print("final output: ", array_in)
-
-
-def quickSort(array, l, r):
-    pivot = array[l]
-    index = l + 1
-    for j in range(index, r):
-        if array[j] < pivot:
-            mem = array[j]
-            array[j] = array[index]
-            array[index] = mem
-            index += 1
-    array[l] = array[index - 1]
-    array[index - 1] = pivot
-    quickSort(array, l, index - 1)
-    quickSort(array, index, r)
+    right = len(array_in) - 1
+    quick_sort(array_in, left, right)
+    print("final output:\n", array_in)
 
 main()
