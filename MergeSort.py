ee56ca9d88084e727f00821904021d43e99f0b98
jfc4050 <jfchiu@uw.edu>
2017-09-16 12:23:19 -0700

    9/16/17 12:23PM

--- MergeSort.py
+++ MergeSort.py
@@ -1,14 +1,15 @@
 def main():
     list_to_sort = [0, 7, 2, 4, 6, 5, 1, 3]
-    print(merge_sort(list_to_sort))
+    print("sorted list:\n", merge_sort(list_to_sort))
 
 
 def merge_sort(lst):
     if len(lst) > 1:
         middle = len(lst) // 2
+
+        # define and recurse on first and second halves
         first_half = lst[:middle]
         second_half = lst[middle:]
-
         merge_sort(first_half)
         merge_sort(second_half)
 
