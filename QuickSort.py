c18e42f961e95bc275fcb75092379464e99f86b6
jfc4050 <jfchiu@uw.edu>
2017-09-16 12:16:10 -0700

    9/16/17 11:58AM quicksort now partitions around randomized pivots

--- QuickSort.py
+++ QuickSort.py
@@ -1,3 +1,6 @@
+import random
+
+
 def swap(array, index1, index2):
     # swap values of indices 1 and 2 in array
     mem = array[index1]
@@ -5,16 +8,29 @@ def swap(array, index1, index2):
     array[index2] = mem
 
 
+def find_pivot(array, left, right):
+    # find random pivot, then moves it to first position in array
+    pivot_index = random.randrange(left, right + 1)
+    pivot = array[pivot_index]
+    swap(array, left, pivot_index)
+
+    pivot_boundary = left + 1
+
+    return pivot, pivot_boundary
+
+
 def quick_sort(array, left, right):
     if (right - left) > 0:
-        pivot = array[left]
-        # pivot_boundary = array.index(pivot) + 1
-        pivot_boundary = left + 1
 
+        pivot, pivot_boundary = find_pivot(array, left, right)
+
+        # iterate through array, moving elements < pivot behind
+        # pivot_boundary, incrementing pivot_boundary as necessary
         for i in range(pivot_boundary, right + 1):
             if array[i] < pivot:
                 swap(array, i, pivot_boundary)
                 pivot_boundary += 1
+
         # put pivot in final position (behind pivot boundary)
         swap(array, array.index(pivot), pivot_boundary - 1)
 
@@ -24,7 +40,7 @@ def quick_sort(array, left, right):
 
 
 def main():
-    array_in = [3, 8, 2, 5, 1, 4, 7, 6, 10, 3, 2]
+    array_in = [3, 8, 2, 5, 1, 4, 7, 6, 10, 3, 2, 5]
     left = 0
     right = len(array_in) - 1
     quick_sort(array_in, left, right)
